ad19430105dac41982eb9dd81fa6d2ed50eed890
Alexis Cepeda <alexkiller0408@hotmail.com>
2025-12-07 12:05:37 -0500

    eso titlin

--- app/Http/Controllers/PublicProductController.php
+++ app/Http/Controllers/PublicProductController.php
@@ -14,8 +14,11 @@ class PublicProductController extends Controller
 
     public function show($id)
     {
-
         $p = PublicProduct::where('_id', (int)$id)->first(); //puede ser tambien id (tambien no funciona)
+        if(!$p){
+            return "no existe";
+
+        }
          return $p;
     }
 }
